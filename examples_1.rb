7dfb39e416929c6588a0af2e9afd2454264a8008
Nagy Zsuzsanna <nagy.zsuzsanna@ejogseged.hu>
2019-01-02 11:30:55 +0100

    feladat 1

--- examples_1.rb
+++ examples_1.rb
@@ -5,6 +5,26 @@ array = [ 1, 2, 3, 4, 567, 123, 356, 654647 ]
 # 3. keep_if
 # 4. tesztőleges
 
+def sorting_with_each arr
+  new_array = []
+  arr.each do |num|
+    new_array << num if num.even?
+  end
+  new_array
+end
+
+p sorting_with_each(array)
+
+def sorting_with_inject arr
+  arr.inject([]) do |new_array, num|
+    new_array << num if num.even?
+    new_array
+  end
+end
+
+p sorting_with_inject(array)
+
+p array.select { |num| num.even? }
 
 # II. Készíts egy olyan asszociatív tömböt (Hash-t), aminek két kulcsa van a következőnek megfelelően:
 # { :paros => [ ... ], :paratlan => [ ... ] }
@@ -13,6 +33,20 @@ array = [ 1, 2, 3, 4, 567, 123, 356, 654647 ]
 # 1. inject
 # 2. tetszőleges
 
+def create_hash arr
+  arr.inject({}) do |new_hash, num|
+    paros ||= []
+    paratlan ||= []
+    if num.even?
+     new_hash[paros] << num
+    else
+      new_hash[paratlan] << num
+    end
+    new_hash
+  end
+end
+
+p create_hash(array)
 
 # III. Készíts egy olyan tömböt, ami a megadott tömbben található számok (Fixnum példányok) String megfelelőjét tartalmazza.
 array = [ 1, 2, 3, 4, 567, 123, 356, 654647 ]
